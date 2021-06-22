b636abf2a238f3602ef0504f37ff2b1a6cec85dc
o-henry-coder <75783011+o-henry-coder@users.noreply.github.com>
2021-06-22 13:10:26 +0300

    Update test_brew.py

--- test_brew.py
+++ test_brew.py
@@ -11,7 +11,7 @@ def test_installing_positive(install_pack) :
 def test_installing_negative(inv_package) :
     assert installing(inv_package) is False
 
-
+"""Check if the package exists (without installing)"""
 @pytest.mark.parametrize("installed_package", ['ffmpeg', 'sqlite', 'htop'])
 def test_check_return_code_0(installed_package) :
     installing(installed_package)
@@ -22,14 +22,14 @@ def test_check_return_code_0(installed_package) :
 def test_check_return_code_256(inv_package) :
     assert check_return_code(inv_package) != 0
 
-
+"""Check installation of the non-existing package"""
 @pytest.mark.parametrize("inv_package", ['ffm5peg', 'tc5p', 'ahtop'])
 def test_not_existing_package(inv_package) :
     with pytest.raises(NotExistingPackage) as excinfo :
         not_existing_package(inv_package)
     assert str(excinfo.value) == "No formulae found"
 
-
+"""Check installation of the previously installed package"""
 def test_installing_existing_package() :
     installing('yarn')
     with pytest.raises(AlreadyInstalledPackage) :
