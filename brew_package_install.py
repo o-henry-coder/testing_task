a3871b2ee9341a22785ab3fb4abfc44fe1a449f6
o-henry-coder <75783011+o-henry-coder@users.noreply.github.com>
2021-06-22 12:30:18 +0300

    Add files via upload

---
+++ brew_package_install.py
@@ -0,0 +1,84 @@
+import os
+import subprocess
+
+path = '/usr/local/bin/brew'
+command = 'install'
+
+
+class NotExistingPackage(Exception) :
+    pass
+
+
+class AlreadyInstalledPackage(Exception) :
+    pass
+
+
+class InvalidCommandUsing(Exception) :
+    pass
+
+
+def installing(package) :
+    subprocess.run([path, command, package])
+    check_path = f"/usr/local/bin/{package}"
+    if os.path.exists(check_path) :
+        check_path = True
+    else :
+        check_path = False
+    return check_path
+
+
+def check_return_code(package_name) :
+    ret_code = os.system(f'brew list | grep {package_name} ')
+
+    return ret_code
+
+
+def not_existing_package(package) :
+    result = subprocess.Popen([path, command, package], stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    err = result.communicate()
+    a = 'No similarly named formulae found'.encode()
+    if a in err[1] :
+        raise NotExistingPackage("No formulae found")
+
+
+def installing_existing_package(package) :
+    result = subprocess.Popen([path, command, package], stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    err = result.communicate()
+    a = 'is already installed and up-to-date'.encode()
+    if a in err[1] :
+        raise AlreadyInstalledPackage("The package is installed")
+
+
+def check_process_error(package) :
+    try :
+        result = subprocess.check_output([path, command, package])
+    except subprocess.CalledProcessError as e :
+        result = e.output
+        raise ChildProcessError
+
+    return result
+
+
+def check_timeout_error(package) :
+    filename = f"/usr/local/bin/{package}"
+    if os.path.exists(filename) :
+        os.system(f"/usr/local/bin/brew uninstall {package}")
+    try :
+        result = subprocess.check_output([path, command, package], timeout=4)
+    except subprocess.TimeoutExpired as e :
+        result = e.output
+        raise TimeoutError
+
+    return result
+
+
+def invalid_command(command, package) :
+    result = subprocess.Popen([path, command, package], stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    err = result.communicate()
+    a = 'Unknown command'.encode()
+    if a in err[1] :
+        raise InvalidCommandUsing("Invalid command")
+
---
+++ test_brew.py
@@ -0,0 +1,51 @@
+import pytest
+from testing_task.brew_package_install import *
+
+
+@pytest.mark.parametrize("install_pack", ['ffmpeg', 'htop', 'wget'])
+def test_installing_positive(install_pack) :
+    assert installing(install_pack) is True
+
+
+@pytest.mark.parametrize("inv_package", ['ffm5peg', 'tcp', 'ahtop'])
+def test_installing_negative(inv_package) :
+    assert installing(inv_package) is False
+
+
+@pytest.mark.parametrize("installed_package", ['ffmpeg', 'sqlite', 'htop'])
+def test_check_return_code_0(installed_package) :
+    installing(installed_package)
+    assert check_return_code(installed_package) == 0
+
+
+@pytest.mark.parametrize("inv_package", ['ffm5peg', 'tcp', 'ahtop'])
+def test_check_return_code_256(inv_package) :
+    assert check_return_code(inv_package) != 0
+
+
+@pytest.mark.parametrize("inv_package", ['ffm5peg', 'tc5p', 'ahtop'])
+def test_not_existing_package(inv_package) :
+    with pytest.raises(NotExistingPackage) as excinfo :
+        not_existing_package(inv_package)
+    assert str(excinfo.value) == "No formulae found"
+
+
+def test_installing_existing_package() :
+    installing('yarn')
+    with pytest.raises(AlreadyInstalledPackage) :
+        assert installing_existing_package('yarn')
+
+
+def test_invalid_command() :
+    with pytest.raises(InvalidCommandUsing) :
+        assert invalid_command('isstall', 'htop')
+
+
+def test_check_process_error() :
+    with pytest.raises(ChildProcessError) :
+        assert check_process_error('tcp')
+
+
+def test_check_timeout_error() :
+    with pytest.raises(TimeoutError) :
+        assert check_timeout_error('htop')
